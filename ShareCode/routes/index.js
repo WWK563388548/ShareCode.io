d9ab244d1abb37fb4f92a0161246e094a54697d8
WWK563388548 <wc563388548@gmail.com>
2018-03-20 07:29:32 +0900

    Added contact page and thanks page

--- ShareCode/routes/index.js
+++ ShareCode/routes/index.js
@@ -11,4 +11,13 @@ router.get("/about", function(req, res, next){
   res.render("about", {title: "ShareCode.io - a collaborative editor"});
 });
 
+/* Contact page */
+router.route('/contact')
+.get(function(req, res, next){
+    res.render("contact", {title: "ShareCode.io - a collaborative editor"});
+})
+.post(function(req, res, next){
+    res.render("thanks", {title: "ShareCode.io - a collaborative editor"});
+});
+
 module.exports = router;
