54f1a4697b6cadc387164dbe8d2692dd65cd9bf7
WWK563388548 <wc563388548@gmail.com>
2018-03-19 23:31:38 +0900

    created navbar by bootstrap and simple about page

--- ShareCode/routes/index.js
+++ ShareCode/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ShareCode.io - a collaborative editor' });
+});
+
+/* Get about page. */
+router.get("/about", function(req, res, next){
+  res.render("about", {title: "ShareCode.io - a collaborative editor"});
 });
 
 module.exports = router;
