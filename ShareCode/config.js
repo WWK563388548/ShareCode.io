5b2b86098e275c703981a346cb6519345cca4c44
WWK563388548 <wc563388548@gmail.com>
2018-03-22 03:48:11 +0900

    added new modules

--- ShareCode/config.js
+++ ShareCode/config.js
@@ -8,5 +8,6 @@ module.exports = {
             pass: 'Your email password'
         }
     },
-    dbConnstring: "mongodb://127.0.0.1:27017/sharecode"
+    dbConnstring: "mongodb://127.0.0.1:27017/sharecode",
+    sessionKey: 'ShareCode'
 }
