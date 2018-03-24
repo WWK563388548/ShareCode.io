1cf3c296c5d7de59e76e35d927cd6f8c15613a0b
WWK563388548 <wc563388548@gmail.com>
2018-03-24 10:41:03 +0900

    Created room chat for limit field of chatting

--- ShareCode/routes/task.js
+++ ShareCode/routes/task.js
@@ -23,7 +23,7 @@ router.get('/task/:id', function(req, res){
             }
 
             if(data) {
-                res.render('task', {data: data});
+                res.render('task', {data: data, roomId: data.id});
             } else {
                 res.render('error');
             }
