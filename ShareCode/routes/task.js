0c4a0d4d8d6c6f618c148eab71656a85e5ebce61
WWK563388548 <wc563388548@gmail.com>
2018-03-25 19:29:10 +0900

    Save edited task to db

--- ShareCode/routes/task.js
+++ ShareCode/routes/task.js
@@ -23,7 +23,7 @@ router.get('/task/:id', function(req, res){
             }
 
             if(data) {
-                res.render('task', {data: data, roomId: data.id});
+                res.render('task', {content: data.content, roomId: data.id});
             } else {
                 res.render('error');
             }
--- ShareCode/socket-server.js
+++ ShareCode/socket-server.js
@@ -11,7 +11,13 @@ module.exports = function(server) {
         socket.on('joinRoom', function(data){
             if(!roomList[data.room]) {
                 var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb){
-                    cb(true);
+                    var self = this;
+                    Task.findByIdAndUpdate(data.room, {content: self.document}, function(err) {
+                        if(err) {
+                            return cb(false);
+                        }
+                        cb(true);
+                    });
                 });
                 roomList[data.room] = socketIOServer;
             }
